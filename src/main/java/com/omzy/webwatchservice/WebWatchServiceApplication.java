b53ffe8d34b67f82b1642e5cbeacc664581ef7c2
Matej-Omazic <omazzza@gmail.com>
2024-10-10 17:46:07 +0200

    Spring boot initialized.

---
+++ src/main/java/com/omzy/webwatchservice/WebWatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omzy.webwatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWatchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omzy/webwatchservice/WebWatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omzy.webwatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWatchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
